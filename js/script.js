306978341d4e3aed76887dffa56a96ea36352325
scabreraa <scabrera@limadigital.pe>
2022-12-02 00:12:25 -0500

    subiendo portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// TEXT ANIMATED
+var typed = new Typed(".typing", {
+    strings:["", "Desarrollador Web","Back-end Jr", "Diseño Grafico Digital"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
